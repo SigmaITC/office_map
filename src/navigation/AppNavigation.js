bad3a4cd8419a88b7e54d4b56b7e31a56391a312
Fredrik Lunde <fredrik.lunde3@gmail.com>
2017-11-24 15:36:04 +0100

    added floor_2 image

--- src/navigation/AppNavigation.js
+++ src/navigation/AppNavigation.js
@@ -2,9 +2,9 @@ import { StackNavigator } from 'react-navigation';
 import HomeScreen from '../screens/home_screen'
 import FloorScreen from '../screens/floor_view'
 
-const SimpleApp = StackNavigator({
+const AppNavigation = StackNavigator({
     Home: { screen: HomeScreen },
     Floor: { screen: FloorScreen },
   });
 
-export default SimpleApp
+export default AppNavigation
